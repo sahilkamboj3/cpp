a7117972f04a60f3e7cd48d5261a1a644f1e10af
Sahil Kamboj <kambojsahil327@gmail.com>
2024-02-25 21:58:10 -0600

    Added lock_guard

--- threads.cpp
+++ threads.cpp
@@ -11,6 +11,11 @@ mutex - mutual exclusion (only one thread has access to object)
 deadlock - thread doesn't return lock or tell other threads the lock has been
 released so other threads are stuck waiting since they don't know/can't lock
 the mutex
+
+lock_guard - mutex wrapper where when the destructor is called, any of the
+held mutexes are going to be unlocked, even in the case of exceptions. This
+is preferred over having naked mutexes since it is a source of bugs to
+remember to unlock the mutex in the different flow paths of the function
 */
 
 #include <unistd.h>
@@ -22,11 +27,28 @@ the mutex
 std::mutex globLock;
 static int glob = 0;
 
-void increment_glob()
+void increment_glob_mutex()
 {
 	globLock.lock();
 	glob++;
 	globLock.unlock();
+}
+
+void increment_glob_lockguard()
+{
+	// lock_guard here ensures the globLock global mutex is going to be released
+	// when the lock_guard destructor is called at the end of the function, even
+	// if there is an exception thrown
+	std::lock_guard<std::mutex> lockGuard(globLock);
+	try
+	{
+		glob++;
+		throw "Forced error thrown...";
+	} catch(...)
+	{
+		std::cout << "Exception thrown..." << std::endl;
+		return;
+	}
 }
 
 void DoWork()
@@ -62,11 +84,12 @@ int main()
 	worker.join();
 	lamdaThread.join();
 
-	const int NUM_THREADS = 1000;
+	const int NUM_THREADS = 100;
 	std::vector<std::thread> threads;
 	for (int i = 0; i < NUM_THREADS; i++)
 	{
-		threads.push_back(std::thread(&increment_glob));
+		// threads.push_back(std::thread(&increment_glob_mutex));
+		threads.push_back(std::thread(&increment_glob_lockguard));
 	}
 
 	for (int i = 0; i < NUM_THREADS; i++)
